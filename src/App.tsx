2004fc9c67f32878589f035a29cedc8ce4c89f89
supermanisme2002 <bthanh2001@gmail.com>
2023-05-06 23:25:26 +0700

    add and config admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routes';
-import "./assets/styles/app.scss"
-import { Provider } from 'react-redux';
-import { setupInterceptor } from './services/request';
-import store from './store/configureStore';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import "./assets/styles/app.scss";
+import { Provider } from "react-redux";
+import { setupInterceptor } from "./services/request";
+import store from "./store/configureStore";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ThemeProvider from "./admin/theme";
+import { StyledChart } from "./admin/components/chart";
+import ScrollToTop from "./admin/components/scroll-to-top/ScrollToTop";
+import { HelmetProvider } from "react-helmet-async";
 
-setupInterceptor(store)
+setupInterceptor(store);
 
 function App() {
   return (
-   <Provider store={store}>
-     <BrowserRouter>
-        <Router />
+    <Provider store={store}>
+      <BrowserRouter>
+        <HelmetProvider>
+          <ThemeProvider>
+            <ScrollToTop />
+            <StyledChart />
+            <Router />
+          </ThemeProvider>
+        </HelmetProvider>
         <ToastContainer />
-     </BrowserRouter>
-   </Provider>
+      </BrowserRouter>
+    </Provider>
   );
-} 
+}
 
 export default App;
